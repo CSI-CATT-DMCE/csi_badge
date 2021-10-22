d7872f2142117c60b45eb6e6d5e46832efc9ff1f
Yash Nerkar <65919365+yash-png@users.noreply.github.com>
2021-10-22 17:17:14 +0530

    Update app.js

--- node/app.js
+++ node/app.js
@@ -30,7 +30,7 @@ console.log(process.env.DB_HOST);
 app.get('/', async (req, res,next) => {
 // leaderboard start
 var connection = mysql.createConnection({
-	host: process.env.DB_HOST
+	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_name,
